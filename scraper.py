bb5ad024381c2f5edbba2956bb56cb95406456a8
ferizoozoo <farhadzohoorz@gmail.com>
2020-04-25 16:16:07 +0430

    Make a function for returning info about each country

--- scraper.py
+++ scraper.py
@@ -1,4 +1,25 @@
 import bs4
+import requests
 
-def scrap():
-	pass
+website_addr = 'https://www.worldometers.info/coronavirus/'
+
+def scrap(addr):
+	"""Return the BeautifulSoup instance of the website address.
+	"""
+	res = requests.get(addr)
+	siteSoup = bs4.BeautifulSoup(res.text, 'html.parser')
+	return siteSoup
+
+def allCountries():
+	"""Return a list of all countries with their statistics.
+	"""
+	soup = scrap(website_addr)
+	countries = []
+
+def eachCountry(country):
+	"""Return the data of each country as a dictionary.
+	"""
+	country_addr = website_addr + f'country/{country}' 
+	soup = scrap(country_addr)
+	country_data = soup.find_all(attrs = { 'id': 'maincounter-wrap' })
+	return {country_data[i].h1.text[:-1].strip() : country_data[i].span.text.strip() for i in range(len(country_data))}
